9d1dc1f9f78e99caf9c9e2ef021a42baef13e23f
Mark Campbell <artsmc88@gmail.com>
2015-12-18 13:44:28 -0500

    updated files in repo

--- js/core.js
+++ js/core.js
@@ -4,16 +4,7 @@
 // except for 'app' ones, which are in a sibling
 // directory.
 requirejs.config({
-	 urlArgs: "bust=" + (new Date()).getTime(),
-    baseUrl: 'js/libs/',
-      shim: {
-    d3: { exports: 'd3' },
-    topojson: { exports: 'topojson' }
-  },
-  paths: {
-    'd3': 'd3/d3.min',
-    'topojson': 'd3/topojson'
-  }
+
 });
 
 // Start loading the main app file. Put all of
